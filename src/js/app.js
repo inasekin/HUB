a757c6e43b0360e4d1c656ed975782c36910855e
Ivan Nasekin <nasekin@ipavlov.ai>
2022-01-17 01:51:00 +0300

    finish hero-block desktop version

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,16 @@
 import * as flsFunctions from "./modules/functions.js";
 import {faqFunction} from "./modules/faq.js";
+import {preload, csaHead, images} from "./modules/showHero.js";
+
+preload(images.slice(0), function() {
+  requestAnimationFrame(csaHead);
+});
 
 faqFunction();
+
 flsFunctions.isWebp();
+
+
 
 /*
 import Swiper, { Navigation, Pagination } from 'swiper';
