bdbac44fb78d0b87981ec542dcd674e80c9fa7eb
Nasekin <nasekinwhiteorblack@gmail.com>
2022-01-16 22:36:16 +0300

    made layout faq-block

--- src/js/modules/faq.js
+++ src/js/modules/faq.js
@@ -1,3 +1,35 @@
 export const faqFunction = () => {
+    const accordion = document.querySelector('.faq-block__accordion');
+    const items = accordion.querySelectorAll('.faq-block__accordion-item');
 
+    items.forEach((item) => {
+        const title = item.querySelector('.faq-block__accordion-title');
+
+        title.addEventListener('click', (e) => {
+            const opened_item = accordion.querySelector('.is-open');
+
+            // Toggle current item
+            toggle_item(item);
+
+            // Close earlier opened item if exists
+            if (opened_item && opened_item !== item) {
+                toggle_item(opened_item);
+            }
+
+
+        });
+    });
+
+    const toggle_item = (item) => {
+        const body = item.querySelector('.faq-block__accordion-body');
+        const content = item.querySelector('.faq-block__accordion-content');
+
+        if (item.classList.contains('is-open')) {
+            body.removeAttribute('style');
+            item.classList.remove('is-open');
+        } else {
+            body.style.height = body.scrollHeight + 'px';
+            item.classList.add('is-open');
+        }
+    }
 }
